554acf316a3aa2ab6c17e247b55df5852674b1be
slbsla <lachihebsamir@yahoo.fr>
2021-03-14 19:01:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-example-promise-t6xys6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-example-promise-t6xys6)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    var promise = doAsyncTask();
+    promise
+      .then(() => {
+        console.log("promised");
+      })
+      .catch(error => {
+        console.log("error in promise");
+      })
+      .finally(() => {
+        console.log("finished");
+      });
+    console.log("contunie working");
+  }
+
+  name = "Angular " + VERSION.major;
+}
+
+const hasError = false;
+
+function doAsyncTask() {
+  var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Async Work Complete");
+      resolve();
+    }, 5000);
+  });
+  return promise;
+}
+
+function resolve() {
+  console.log("resolve");
+}
+
+function reject() {
+  console.log("reject");
+}
